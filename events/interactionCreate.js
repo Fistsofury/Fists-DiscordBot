5efa4b5ec7739ce3fe8d9a6e760160f1766a1832
Fistsofury <92045818+Fistsofury@users.noreply.github.com>
2023-11-09 12:38:15 +0000

    added role permission
    Ability to lock down via role permissions

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,11 +1,20 @@
+const { PermissionFlagsBits } = require('discord.js');
+const config = require('../config.json');
+
 module.exports = {
   name: 'interactionCreate',
   async execute(interaction, client) {
-    if (!interaction.isCommand()) return;
+    if (!interaction.isChatInputCommand()) return; 
 
     const command = client.commands.get(interaction.commandName);
 
     if (!command) return;
+    const memberRoles = interaction.member.roles.cache;
+    const hasPermission = config.allowedRoles.some(roleId => memberRoles.has(roleId));
+
+    if (!hasPermission) {
+      return interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+    }
 
     try {
       await command.execute(interaction);
